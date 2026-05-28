0fab2624f771cef5c10fa2088fbde806278cb087
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2026-05-28 12:18:37 +0800

    refactor: clean up guest article routing

--- routes/api/guest.php
+++ routes/api/guest.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('about', [AboutController::class, 'show']);
 Route::get('articles', [ArticleController::class, 'index']);
-Route::get('articles/{article}', [ArticleController::class, 'show']);
+Route::get('articles/{article:slug}', [ArticleController::class, 'show']);
 Route::get('businesses', [BusinessController::class, 'index']);
 Route::post('complaints', [ComplaintController::class, 'store']);
 Route::get('organizational-structure', [OrganizationalStructureController::class, 'index']);
